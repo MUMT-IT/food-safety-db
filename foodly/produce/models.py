e86513de4611d24ffe72f668ca9bd4e27a17a87d
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-22 15:46:50 +0700

    updated farm models
    added swap file to gitignore
    
    added another migration

--- foodly/produce/models.py
+++ foodly/produce/models.py
@@ -7,16 +7,30 @@ class Person(models.Model):
     gender = models.PositiveIntegerField()  # 0 male, 1 female
     updated_at = models.DateTimeField(auto_now=True)
     added_at = models.DateTimeField(auto_now_add=True)
+    relatives = models.ManyToManyField("self")
 
     def __unicode__(self):
         return u"{} {}".format(self.firstname, self.lastname)
 
 
 class Farm(models.Model):
-    owner = models.ManyToManyField(Person,
+    owners = models.ManyToManyField(Person,
             related_name='farms',
             related_query_name='farm')
-    estimated_size = models.IntegerField(blank=True, null=True)
+    estimated_size = models.DecimalField(blank=True, null=True,
+                        max_digits=5, decimal_places=2)
+    estimated_owned_size = models.DecimalField(blank=True, null=True,
+                        max_digits=5, decimal_places=2)
+    estimated_leased_size = models.DecimalField(blank=True, null=True,
+                        max_digits=5, decimal_places=2)
+    duration = models.IntegerField(blank=True, null=True)
 
     def __unicode__(self):
         return u"Estimated size={}".format(self.estimated_size)
+
+
+class AgriType(models.Model):
+    agtype = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return u"{}".format(self.agtype)
