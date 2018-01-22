4a563514130c0b7d3513f6b9fab0b26f4afe5942
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-22 15:49:33 +0700

    registered Agritype in django admin

--- foodly/produce/admin.py
+++ foodly/produce/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from produce.models import Farm, Person
+from produce.models import Farm, Person, AgriType
 
 admin.site.register(Farm)
 admin.site.register(Person)
+admin.site.register(AgriType)
